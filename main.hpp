dad19a5b9fdf578912a0263069199514afd8f5be
Ramimami7 <rhabbal22@gmail.com>
2026-05-12 11:18:55 -0700

    main.hpp

--- main.hpp
+++ main.hpp
@@ -1,32 +1,92 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-
 #include <iostream>
-#include <cstring>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int finduserstring(char[], int, char[], int);
+const int N = 5000;
 
-int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
+int makeNameRecord(string state[], string gender[], int year[], string name[], int count[])
 {
-    for (int i = 0; i <= cstrlen - userlen; i++)
+    ifstream infile("babyname.txt");
+
+    if (!infile)
     {
-        int j = 0;
+        cout << "Error: could not open babyname.txt" << endl;
+        return 0;
+    }
 
-        while (j < userlen && cstr[i + j] == userstr[j])
-        {
-            j++;
-        }
+    int cnt = 0;
 
-        if (j == userlen)
+    while (infile >> state[cnt] >> gender[cnt] >> year[cnt] >> name[cnt] >> count[cnt])
+    {
+        cnt++;
+        if (cnt >= N)
+            break;
+    }
+
+    infile.close();
+    return cnt;
+}
+
+void printoutcontents(string state, string gender, int year, string name, int count)
+{
+    cout << "State: "  << state  << "  "
+         << "Gender: " << gender << "  "
+         << "Year: "   << year   << "  "
+         << "Name: "   << name   << "  "
+         << "Count: "  << count  << endl;
+}
+
+void printoutallrecords(int cnt, string state[], string gender[], int year[], string name[], int count[])
+{
+    cout << "=== All Records (" << cnt << " total) ===" << endl;
+
+    for (int i = 0; i < cnt; i++)
+    {
+        printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
+    }
+}
+
+int findNames(int cnt,
+              string state[], string gender[], int year[],
+              string name[],  int count[],
+              char starting,  string stname)
+{
+    int found = 0;
+
+    cout << "=== Names starting with '" << starting
+         << "' in state '" << stname << "' ===" << endl;
+
+    for (int i = 0; i < cnt; i++)
+    {
+        if (state[i] == stname && name[i][0] == starting)
         {
-            return i;
+            printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
+            found++;
         }
     }
 
-    // userstr was not found in cstr
-    return -1;
+    cout << "Total records found: " << found << endl;
+    return found;
+}
+
+int main()
+{
+    string state[N], gender[N], name[N];
+    int    year[N],  count[N];
+
+    int cnt = makeNameRecord(state, gender, year, name, count);
+    cout << "Records loaded: " << cnt << endl << endl;
+
+    printoutallrecords(cnt, state, gender, year, name, count);
+    cout << endl;
+
+    findNames(cnt, state, gender, year, name, count, 'J', "CA");
+
+    return 0;
 }
 
 #endif
